a825e3271b02a7685f0273038f2dfd10e61ff42a
linsaraj <linsaraj0@gmail.com>
2024-11-28 02:48:41 -0600

    min heap visualization wop

--- proj3-frontend/src/App.js
+++ proj3-frontend/src/App.js
@@ -12,7 +12,49 @@ const labelInput = {
   letterSpacing: "3px",
 };
 
+const visualizeMinHeap = (arr, index = 0, position = 0, level = 0) => {
+  // if min heap empty, return nothing
+  if(index >= arr.length){
+    return null;
+  }
+
+  // get positions for children of current node
+  const left = (2 * index) + 1;
+  const right = (2 * index) + 2;
+
+  const spacing = 200 / (level + 1);
+
+  return (
+    // display current node
+    <div>
+      <div style={{ position: 'relative', marginLeft: position}}>
+        <div
+          style={{
+            width: 50,
+            height: 50,
+            backgroundColor: '#FFFFFF',
+            color: 'black',
+            textAlign: 'center',
+            lineHeight: '50px',
+            borderRadius: '50%',
+          }}>
+          {arr[index]}
+        </div>
+      </div>
+
+      {/* display children nodes recursively*/}
+      <div style={{ display: 'flex' }}>
+        {visualizeMinHeap(arr, left, position - spacing, level + 1)}
+        {visualizeMinHeap(arr, right, position + spacing, level + 1)}
+      </div>
+    </div>
+  );
+};
+
 function App() {
+
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -37,6 +79,10 @@ function App() {
             Option 3
           </Dropdown.Item>
         </DropdownButton>
+
+        <div style={{marginTop: '50px'}}>
+          {visualizeMinHeap(arr)}
+        </div>
 
       </header>
     </div>
