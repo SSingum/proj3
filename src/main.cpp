cb8299a6f8a66aa7ea2e31ef7b435629548e2229
linsaraj <linsaraj0@gmail.com>
2024-12-03 02:28:58 -0500

    added dollar sign to revenue

--- src/main.cpp
+++ src/main.cpp
@@ -214,14 +214,14 @@ int main() {
             int counter = 0;
             for (auto item : quickSelectResult){
                 counter += 1;
-                cout << "\033[3m" << counter << ") Company Name: " << item.getName() << " Revenue: " << fixed << setprecision(2) << item.getRevenue() << "\033[23m" << endl;
+                cout << "\033[3m" << counter << ") Company Name: " << item.getName() << " Revenue: " << "$" << fixed << setprecision(2) << item.getRevenue() << "\033[23m" << endl;
             }
         } else if (input == 'h'){
             heapSelectResult = heapSelect(totalRev, k);
             int counter = 0;
             for (auto item : heapSelectResult){
                 counter += 1;
-                cout << "\033[3m" << counter << ") Company Name: " << item.getName() << " Revenue: " << fixed << setprecision(2) << item.getRevenue() << "\033[23m" << endl;
+                cout << "\033[3m" << counter << ") Company Name: " << item.getName() << " Revenue: " << "$" << fixed << setprecision(2) << item.getRevenue() << "\033[23m" << endl;
             }
         }
 
